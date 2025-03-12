55e625c5e1872179a16359ecb6b2b2705ce85749
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 15:59:01 +0700

    menambahkan class kodeProduk

---
+++ KodeProduk.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_103022300016
+{
+    internal class KodeProduk
+    {
+        public enum produk // Pembuatan Enum
+        {
+            Laptop, Smartphone, Tablet, Headset, Keyboard, Mouse, Printer, Monitor, Smartwatch, Kamera
+        };
+
+        public static String getKodeProduk(produk Produk)
+        // Pembuatan method getKodeProduk Static dengan parameter produk
+        {
+            String[] kodeProduk = {"E100", "E101", "E102", "E103", "E104", "E105", "E106", "E107", "E108", "E109" }; // Array berisi data kodeProduk
+            return kodeProduk[(int)Produk]; // Mengembalikan nilai kodeProduk sesuai dengan index Produk
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace modul4_103022300016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Daftar Produk"); // Fungsi Header
+            Console.WriteLine("----------------------------------------"); // Fungsi Header
+            Console.WriteLine("Produk Elektronik \t | Kode Produk"); // Fungsi Header
+            Console.WriteLine("----------------------------------------"); // Fungsi Header
+            foreach (KodeProduk.produk produk in Enum.GetValues(typeof(KodeProduk.produk))) // Perulangan menunjukkan enum
+            {
+                String namaProduk = produk.ToString(); // Konversi Enum ke string
+                Console.WriteLine($"{produk, -24} | {KodeProduk.getKodeProduk(produk)}"); // Print nama produk dan kode produk
+            }
+        }
+    }
+}
